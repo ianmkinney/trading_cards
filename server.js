7c9cd8eec0f09ecd6cc5a05da4771483c9ae0781
ianmkinney <ikinney51@gmail.com>
2021-10-18 12:54:15 -0400

    Completed buy route

--- server.js
+++ server.js
@@ -37,15 +37,20 @@ app.get('/:id/pack', async (req,res) => {
 })
 
 app.get('/:id/pack/:cardid/buy', async (req,res) => {
-    let userID = req.params.id;
-    let cardID = req.params.cardid;
-    let user = await Collector.findByPk(userID);
-    let selectedCard = await Card.findByPk(cardID);
-    if(user.budget > selectedCard.price) {
+    const userID = req.params.id;
+    const cardID = req.params.cardid;
+    const user = await Collector.findByPk(userID);
+    const selectedCard = await Card.findByPk(cardID);
+    if(user.budget > selectedCard.price && selectedCard.CollectorId == null) {
         user.budget = user.budget - selectedCard.price
-        user.save()
+        await user.addCard(selectedCard)
+        await user.save()
+        await selectedCard.save()
         res.send(`<h1>${selectedCard.name} was purchased for ${selectedCard.price}!</h1>
                   <h2>${user.name}'s budget is at $${user.budget}</h2>`)
+    } else if(selectedCard.CollectorId != null){
+        conflictUser = await Collector.findByPk(selectedCard.CollectorId)
+        res.send(`<h1>Card belongs to ${conflictUser.name}<h1>`)
     } else {
         res.send(`<h1>Card is too expensive<h1>`)
     }
